f6354a1aed4d25d192121318a98298ef0c11f3d9
Rainer Stropek <rainer@software-architects.at>
2022-03-15 10:26:55 +0100

    Add example for IEnumerable and yield

---
+++ HelloYield/HelloYield/Program.cs
@@ -0,0 +1,50 @@
+﻿var myNumbers = GetNumbersBetter();
+var filteredNumbers = RemoveOddNumbersBetter(myNumbers);
+foreach(var number in filteredNumbers)
+{
+    Console.WriteLine(number);
+}
+
+IEnumerable<int> GetNumbers()
+{
+    var numbers = new List<int>();
+    for (var i = 0; i < 10; i++)
+    {
+        numbers.Add(i);
+    }
+
+    return numbers;
+}
+
+IEnumerable<int> GetNumbersBetter()
+{
+    for (var i = 0; i < 10; i++)
+    {
+        yield return i;
+    }
+}
+
+IEnumerable<int> RemoveOddNumbers(IEnumerable<int> numbers)
+{
+    var result = new List<int>();
+    foreach (var number in numbers)
+    {
+        if (number % 2 == 0)
+        {
+            result.Add(number);
+        }
+    }
+
+    return result;
+}
+
+IEnumerable<int> RemoveOddNumbersBetter(IEnumerable<int> numbers)
+{
+    foreach (var number in numbers)
+    {
+        if (number % 2 == 0)
+        {
+            yield return number;
+        }
+    }
+}
