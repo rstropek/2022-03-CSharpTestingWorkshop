c5e8bea3e07c8bc92185755aa57c4c640642adbd
Rainer Stropek <rainer@software-architects.at>
2022-03-15 10:27:06 +0100

    Complete Advent of Code, Day 1

--- AdventOfCode2022/AdventOfCode2022.Day01/Program.cs
+++ AdventOfCode2022/AdventOfCode2022.Day01/Program.cs
@@ -3,15 +3,20 @@ var solver = new PuzzleSolver();
 var result = solver.SolvePart1(lines);
 Console.WriteLine(result);
 
+var result2 = solver.SolvePart2(lines);
+Console.WriteLine(result2);
+
 public class PuzzleSolver
 {
     public int SolvePart1(IEnumerable<string> lines)
+        => SolvePart1(lines.Select(line => int.Parse(line)));
+
+    public int SolvePart1(IEnumerable<int> values)
     {
         int? previous = null;
         var increaseCount = 0;
-        foreach (var line in lines)
+        foreach (var number in values)
         {
-            var number = int.Parse(line);
             if (previous.HasValue && number > previous)
             {
                 increaseCount++;
@@ -35,7 +40,17 @@ public class PuzzleSolver
     }
 
     public int SolvePart2(IEnumerable<string> lines)
-    {
+        => SolvePart1(GetWindows(lines));
 
+    public IEnumerable<int> GetWindows(IEnumerable<string> lines)
+        => GetWindows(lines.Select(line => int.Parse(line)));
+
+    public IEnumerable<int> GetWindows(IEnumerable<int> numbers)
+    {
+        var numbersArray = numbers.ToArray();
+        for (var i = 0; i < numbersArray.Length - 2; i++)
+        {
+            yield return numbersArray[i] + numbersArray[i + 1] + numbersArray[i + 2];
+        }
     }
 }
